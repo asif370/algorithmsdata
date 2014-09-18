b9db29c70608c728dd66843fd0c345b5c2132c63
Ryan D'souza <dsouzarc@gmail.com>
2014-09-17 21:18:18 -0400

    Constructor, getter, setter

--- RationalNumber.java
+++ RationalNumber.java
@@ -1,6 +1,32 @@
-public class RationalNumber { 
+public final class RationalNumber { 
   /** Written by Ryan D'souza
     * Exercise 16: http://algs4.cs.princeton.edu/12oop/ */
   
+  private long numerator;
+  private long denominator;
+ 
+  public RationalNumber(final long numerator, final long denominator) { 
+    this.numerator = numerator;
+    this.denominator = denominator;
+  }
   
+  @Override
+  public boolean equals(final Object other) { 
+    if(!(other instanceof RationalNumber)) { 
+      return false;
+    }
+    
+    final RationalNumber otherNumber = (RationalNumber) other;
+    
+    if(otherNumber == this) { 
+      return true;
+    }
+    
+    return false;
+  }
+  
+  @Override
+  public String toString() { 
+    return numerator + "/" + denominator;
+  }
 }
