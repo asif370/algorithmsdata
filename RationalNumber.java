78b51abb4a54feff74fb1f4a8c3120e504bc0140
Ryan D'souza <dsouzarc@gmail.com>
2014-09-17 21:30:15 -0400

    Simplify method

--- RationalNumber.java
+++ RationalNumber.java
@@ -10,8 +10,24 @@ public final class RationalNumber {
     this.denominator = denominator;
   }
   
+  /** Simplifies the fraction by dividing both numbers by the GCD */
+  public void simplify() { 
+    final long GCD = this.getGCD();
+    if(GCD == 1 || GCD == 0) { 
+      return;
+    }
+    
+    this.numerator /= GCD;
+    this.denominator /= GCD;
+  }
+  
   /** Returns Greatest Common Denominator, used for simplifying */
-  private long getGCD(long numerator, long denominator) { 
+  public long getGCD() { 
+    return getGCD(this.numerator, this.denominator);
+  }
+  
+  /** Returns Greatest Common Denominator, used for simplifying */
+  private static long getGCD(long numerator, long denominator) { 
     //In case of negatives
     numerator = Math.abs(numerator);
     denominator = Math.abs(denominator);
@@ -19,7 +35,6 @@ public final class RationalNumber {
     if(denominator == 0) { 
       return numerator;
     }
-    
     return getGCD(denominator, numerator % denominator);
   }
   
@@ -52,5 +67,11 @@ public final class RationalNumber {
   @Override
   public String toString() { 
     return numerator + "/" + denominator;
+  }
+  
+  public static void main(String[] ryan) { 
+    final RationalNumber myNum = new RationalNumber(2, 8);
+    myNum.simplify();
+    System.out.println(myNum);
   }
 }
