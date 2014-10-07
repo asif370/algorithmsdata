a9f482294341fb306aa3f9303c2dd311e91229ba
Ryan D'souza <dsouzarc@gmail.com>
2014-10-07 17:57:15 -0400

    Constructor and variables

--- Big_Integer.java
+++ Big_Integer.java
@@ -1,3 +1,12 @@
 public class Big_Integer { 
   /** Class to hold large integers in arrays */
+  private static final int SIZE = 40;
+  
+  private final int[] digits;
+  
+  public Big_Integer() { 
+    this.digits = new int[SIZE];
+  }
+  
+  
 }
