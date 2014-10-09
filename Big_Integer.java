23fec447ebbf391755001bc92a718ea6017858da
Ryan D'souza <dsouzarc@gmail.com>
2014-10-09 18:03:55 -0400

    isEqual method

--- Big_Integer.java
+++ Big_Integer.java
@@ -15,6 +15,11 @@ public class Big_Integer {
     this.digits = parse(number);
   }
   
+  /** Returns the digits */
+  public int[] getDigits() { 
+    return this.digits;
+  }
+  
   /** Returns an int[] of the number (passed as a string */
   public static int[] parse(final String number) { 
     final int[] theNumber = new int[SIZE];
@@ -24,5 +29,23 @@ public class Big_Integer {
       theNumber[startAt] = ((int) digits[i]) - 48;
     }
     return theNumber;
+  }
+  
+  /** Returns true if both integers are equal */
+  @Override
+  public boolean equals(final Object other) { 
+    if(!(other instanceof Big_Integer)) { 
+      return false;
+    }
+    
+    final Big_Integer otherNum = (Big_Integer) other;
+    final int[] otherDigits = otherNum.getDigits();
+    
+    for(int i = 0; i < otherDigits.length; i++) { 
+      if(otherDigits[i] != this.digits[i]) { 
+        return false;
+      }
+    }
+    return true;
   }
 }
