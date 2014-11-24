92b6487c634b6a92b1c48b71915adb50187fdb9b
= <dsouzarc@gmail.com>
2014-11-24 08:28:56 -0500

    Added Merge Sort

--- Sort.java
+++ Sort.java
@@ -3,7 +3,7 @@ import java.util.Arrays;
 
 /**
  * Written by Ryan D'souza
- * A collection of handwritten sorts */
+ * A collection of sorts written by memory */
 
 public class Sort { 
 
@@ -14,6 +14,7 @@ public class Sort {
      * ~N^2 / 2 compares + N exchanges
      * */
     public static void selectionSort(final int[] array) { 
+        final long startTime = System.currentTimeMillis();
         for(int i = 0; i < array.length; i++) { 
             int min = Integer.MAX_VALUE;
             int minLoc = 0;
@@ -27,18 +28,38 @@ public class Sort {
             array[minLoc] = array[i];
             array[i] = min;
         }
+        System.out.println("SelectionSort MS: " + (System.currentTimeMillis() - startTime));
+    }
+
+    /** Perform Insertion sort to sort the array
+     * Performance depends on how sorted array is
+     * Best case: N^2/4 
+     * Worse: N^2/2 */
+    public static void insertionSort(final int[] array) { 
+        final long startTime = System.currentTimeMillis();
+        for(int i = 1; i < array.length; i++) { 
+            for(int j = i; j > 0 && array[j] < array[j - 1];  j--) { 
+                //Swap the variables
+                array[j] ^= array[j - 1];
+                array[j-1] ^= array[j];
+                array[j] ^= array[j - 1];
+            }
+        }
+        System.out.println("InsertionSort MS: " + (System.currentTimeMillis() - startTime));
     }
 
     public static void main(String[] ryan) { 
-        final int NUMELEMENTS = 10;
+        final int NUMELEMENTS = ryan.length > 0 ? Integer.parseInt(ryan[0]) : 100;
 
-        final int[] array = randomArray(NUMELEMENTS); 
-
+        int[] array = randomArray(NUMELEMENTS); 
         System.out.println("Original\t" + Arrays.toString(array));
-
         selectionSort(array);
+        System.out.println("NEW After Selection Sort\t" + Arrays.toString(array));
 
-        System.out.println("\nNEW\t" + Arrays.toString(array));
+        array = randomArray(NUMELEMENTS);
+        System.out.println("\nOriginal\t" + Arrays.toString(array));
+        insertionSort(array);
+        System.out.println("NEW After Insertion Sort\t" + Arrays.toString(array));
     }
 
     /** Returns an array of size num filled with random elements
@@ -51,5 +72,13 @@ public class Sort {
         }
 
         return array;
+    }
+
+    /** Fills array with random elements betweeon 0 and 
+     * size ^3 */
+    public static void randomizeDigits(final int[] array) { 
+        for(int i = 0; i < array.length; i++) { 
+            array[i] = generator.nextInt((int)Math.pow(array.length, 3));
+        }
     }
 }
