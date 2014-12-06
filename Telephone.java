66237a7f13948bcdc667871e011f4cac9a22de15
Ryan D'souza <dsouzarc@gmail.com>
2014-12-06 17:38:08 -0500

    Follows all instructions

--- Telephone.java
+++ Telephone.java
@@ -1,5 +1,9 @@
-import java.util.LinkedList;
-
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
+import java.io.BufferedReader;
+import java.io.FileReader;
 /** Written by Ryan D'souza
  * Algorithms and Data Structures
  * Reads a data set of 1,000 names and numbers from a file
@@ -7,8 +11,13 @@ import java.util.LinkedList;
  * Uses Comparator and binary search for both lookups */
 
 public class Telephone { 
+
+    //Two lists, each holds people in different orders
+    //names = by name, phoneNumbers = by phone number
+    private final ArrayList<Person> names, phoneNumbers;
+
     /** Represents a Person in the phone book */
-    private static class Person { 
+    private static class Person {
         public final String name;
         public final String phoneNumber;
 
@@ -16,5 +25,59 @@ public class Telephone {
             this.name = name;
             this.phoneNumber = phoneNumber;
         }
+
+        /** Comparator for comparing by first name */
+        public static final Comparator<Person> sortByPhone = new Comparator<Person>() { 
+            @Override
+            public int compare(final Person p1, final Person p2) { 
+                return p1.phoneNumber.compareTo(p2.phoneNumber);
+            }
+        };
+
+        /** Comparator for comparing by last name */
+        public static final Comparator<Person> sortByName = new Comparator<Person>() {
+            @Override
+            public int compare(final Person p1, final Person p2) { 
+                return p1.name.compareTo(p2.name);
+            }
+        };
     }
+
+    public Telephone(final String fileName) { 
+        this.names = new ArrayList<Person>();
+        this.phoneNumbers = new ArrayList<Person>();
+        try { 
+            final BufferedReader reader = new BufferedReader(new FileReader(fileName));
+
+            String line = "";
+
+            while((line = reader.readLine()) != null) { 
+                final String phoneNumber = line.substring(0, line.indexOf(" ")).replace(" ", "");
+                final String name = line.substring(line.indexOf(" ") + 1);
+                
+                //Save memory by only using pointers
+                final Person person = new Person(name, phoneNumber);
+                this.names.add(person);
+                this.phoneNumbers.add(person);
+            }
+
+            //Sort respective arrays
+            Collections.sort(this.names, Person.sortByName);
+            Collections.sort(this.phoneNumbers, Person.sortByPhone);
+        }
+
+        catch(Exception e) { 
+            System.out.println("Error\t" + e.toString());
+        }
+    }
+
+    public Telephone(final Person[] people) { 
+        this.names = new ArrayList<Person>(Arrays.asList(people));
+        this.phoneNumbers = new ArrayList<Person>(Arrays.asList(people));
+        
+        //Sort respective arrays
+        Collections.sort(this.names, Person.sortByName);
+        Collections.sort(this.phoneNumbers, Person.sortByPhone);
+    }
+
 }
