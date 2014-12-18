611d69edcbb19bcbea8cc4c944c1b8c7e0ae639c
= <dsouzarc@gmail.com>
2014-12-18 08:25:04 -0500

    Minor additions

--- AutoComplete/BinarySearchDeluxe.java
+++ AutoComplete/BinarySearchDeluxe.java
@@ -4,5 +4,11 @@
  * http://www.cs.princeton.edu/courses/archive/fall14/cos226/assignments/autocomplete.html */
 
 public class Autocomplete { 
+    private final Term[] terms;
 
+    public Autocomplete(final Term[] terms) { 
+        this.terms = terms;
+    }
+
+    //For returning all terms that start with the prefix in descending order of weigh
 }
