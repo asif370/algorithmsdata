9ecc6da9952f234e870a59fc8711b32418897758
Ryan D'souza <dsouzarc@gmail.com>
2014-12-18 19:55:42 -0500

    Beginning BSD

--- AutoComplete/BinarySearchDeluxe.java
+++ AutoComplete/BinarySearchDeluxe.java
@@ -3,12 +3,5 @@
  * BinarySearch for Autocomplete
  * http://www.cs.princeton.edu/courses/archive/fall14/cos226/assignments/autocomplete.html */
 
-public class Autocomplete { 
-    private final Term[] terms;
-
-    public Autocomplete(final Term[] terms) { 
-        this.terms = terms;
-    }
-
-    //For returning all terms that start with the prefix in descending order of weigh
+public class BinarySearchDeluxe { 
 }
