4f7bb16bf587899ed8e12326b1fa76f0966f2461
Ryan D'souza <dsouzarc@gmail.com>
2014-12-18 19:57:49 -0500

    Default methods

--- AutoComplete/BinarySearchDeluxe.java
+++ AutoComplete/BinarySearchDeluxe.java
@@ -1,10 +1,16 @@
+import java.util.Comparator;
+
 /** 
  * Written by Ryan D'souza
  * BinarySearch for Autocomplete
  * http://www.cs.princeton.edu/courses/archive/fall14/cos226/assignments/autocomplete.html */
 
 public class BinarySearchDeluxe { 
-    public static <Key> int firstIndexOf(final Key[] array, Key key, Comparator<Key> comparator>) { 
+    public static <Key> int firstIndexOf(final Key[] array, Key key, Comparator<Key> comparator) { 
+        return 0;
+    }
+
+    public static <Key> int lastIndexOf(final Key[] array, Key key, Comparator<Key> comparator) { 
         return 0;
     }
 }
