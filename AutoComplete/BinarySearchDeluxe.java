9e19828372c5eded7208d87d454f649a1dcd0201
Ryan D'souza <dsouzarc@gmail.com>
2014-12-18 19:56:21 -0500

    Beginning BSD

--- AutoComplete/BinarySearchDeluxe.java
+++ AutoComplete/BinarySearchDeluxe.java
@@ -4,4 +4,7 @@
  * http://www.cs.princeton.edu/courses/archive/fall14/cos226/assignments/autocomplete.html */
 
 public class BinarySearchDeluxe { 
+    public static <Key> int firstIndexOf(final Key[] array, Key key, Comparator<Key> comparator>) { 
+        return 0;
+    }
 }
