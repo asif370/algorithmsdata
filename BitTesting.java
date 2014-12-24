f3940f0ff544763659a371f740acae2835920158
Ryan D'souza <dsouzarc@gmail.com>
2014-12-24 11:07:10 -0500

    Bit increase/decrease

--- BitTesting.java
+++ BitTesting.java
@@ -1,9 +1,22 @@
 public class BitTesting { 
 
-    public static void main(String[] ryan) { 
+    //All bits moved to left by value, 0s added as padding
+    public static int bitIncrease(final int number, final int value) { 
+        //number * 2^value
+        return number << value;
+    }
 
-        int tempVal = 8;
+    //All bits moved to right by value, 0s added as padding
+    public static int bitDecrease(final int number, final int value) { 
+        //number / 2^value
+        return number >> value;
+    }
 
-        System.out.println(tempVal << 2);
+    public static void main(String[] ryan) {
+
+        final int original = 8;
+
+        System.out.println(bitIncrease(original, 3));
+        System.out.println(bitDecrease(original, 3));
     }
 }
