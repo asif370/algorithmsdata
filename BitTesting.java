44942c22d9cbf1f3e6d1c10c7483b7e1e999ba52
Ryan D'souza <dsouzarc@gmail.com>
2014-12-26 17:01:06 -0500

    More binary operators

--- BitTesting.java
+++ BitTesting.java
@@ -12,11 +12,30 @@ public class BitTesting {
         return number >> value;
     }
 
+    //For each bit, returns their '&&' value
+    public static int ampersand(final int number, final int number2) { 
+        //Number =  01001000
+        //Number2 = 10111000
+        //Result =  00001000
+        return number & number2;
+    }
+
+    //For each bit, return their '||' value
+    public static int or(final int number, final int number2) { 
+        //number =  01001000
+        //number2 = 01001000
+        //Result = 11111000
+
+        return number | number2;
+    }
+
     public static void main(String[] ryan) {
 
         final int original = 8;
 
         System.out.println(bitIncrease(original, 3));
         System.out.println(bitDecrease(original, 3));
+        System.out.println(ampersand(72, 184));
+        System.out.println(or(72, 184));
     }
 }
