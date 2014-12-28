d2fbe1a8956c79fecd277261dcb632d03e918364
Ryan D'souza <dsouzarc@gmail.com>
2014-12-28 18:15:57 -0500

    first commit

---
+++ RandomInterview.java
@@ -0,0 +1,14 @@
+/** 
+ * Written by Ryan D'souza
+ * A random bunch of interview question solutions */
+
+public class RandomInterview { 
+
+    //Prints the local min/max from array where n +- 1
+    public static void localMinMax() { 
+    }
+
+    public static void main(String[] ryan) { 
+        localMinMax();
+    }
+}
