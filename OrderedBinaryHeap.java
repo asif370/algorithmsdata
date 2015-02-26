e69e6f173e33f4c69e3721f19e9c713c9f42deb5
= <dsouzarc@gmail.com>
2015-02-26 08:36:20 -0500

    First commit

---
+++ OrderedBinaryHeap.java
@@ -0,0 +1,14 @@
+import java.util.Arrays;
+
+/** 
+ * Written by Ryan D'souza 
+ * Represents an ordered Binary Heap */
+
+public class OrderedBinaryHeap {
+
+    private int[] array;
+
+    public OrderedBinaryHeap(final int size) {
+        this.array = new int[size];
+    }
+}
