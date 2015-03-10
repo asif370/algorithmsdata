b80682af5e7b2fbf8d188415cc2a116cdb025619
Ryan D'souza <dsouzarc@gmail.com>
2015-03-10 02:17:43 -0400

    Nice, clean, working version

--- AirBnBSolution.java
+++ AirBnBSolution.java
@@ -17,7 +17,7 @@ public class AirBnBSolution {
     public static void main(String[] args) {
         String firstPerson = "John,Smith,john.smith@gmail.com,Los Angeles,1";
         String secondPerson = "Jane,Roberts,janer@msn.com,\"San Francisco, CA\",\"2, 3\"";
-        String thirdPerson = "Alexandra \"\"Alex\"\"\",Menendez,alex.menendez@gmail.comdsouzarc@gmail.com,Miami,4";
+        String thirdPerson = "Alexandra \"\"Alex\"\"\",Menendez,alex.menendez@gmail.com,Miami,4";
 
         System.out.println(parseData(firstPerson));
         System.out.println(parseData(secondPerson));
@@ -30,7 +30,53 @@ public class AirBnBSolution {
         if(input.contains("\"")) {
             result.append("List<");
         }
-        result.append(input.substring(last + 1));
+
+        boolean openQuotes = false;
+
+        for(int i = 0; i < input.length(); i++) {
+            if(!openQuotes && i + 2 < input.length() && input.substring(i, i + 2).equals("\"\"")) {
+                for(int y = i + 1; y < input.length() - 3; y++) {
+                    if(input.substring(y, y + 3).equals("\"\"\"")) {
+                        result.append(input.charAt(y));
+                        openQuotes = false;
+                        i = y + 3;
+                        y = input.length();
+                    }
+
+                    else {
+                        if(input.charAt(y) == ',') {
+                            result.append("|");
+                        }
+                        else {
+                            result.append(input.charAt(y));
+                        }
+                    }
+                }
+            }
+            if(!openQuotes && input.charAt(i) == '\"') {
+                for(int y = i + 1; y < input.length(); y++) {
+                    if(input.charAt(y) == '\"') {
+                        openQuotes = true;
+                        i = y + 1;
+                        y = input.length();
+                    }
+                    else {
+                        result.append(input.charAt(y));
+                    }
+                }
+                result.append("|");
+            }
+
+            else if(!openQuotes && input.charAt(i) == ',') {
+                result.append("|");
+            }
+            else {
+                if(input.charAt(i) != '\"') {
+                    result.append(input.charAt(i));
+                }
+            }
+
+        }
 
         if(input.contains("\"")) {
             result.append('>');
