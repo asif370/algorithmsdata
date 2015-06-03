6d85817e0d5e020b29fe08b2509e99d06818bec0
Ryan D'souza <dsouzarc@gmail.com>
2015-06-02 22:13:49 -0400

    Game version complete. Still needs prettifying and minor touches for when time has elapsed

--- Typing/TypeRecorder.java
+++ Typing/TypeRecorder.java
@@ -1,123 +1,158 @@
 import javax.swing.*;
+import java.io.*;
 import java.awt.*;
 import java.util.*;
- 
+
 public class TypeRecorder extends JFrame {
- 
+
     private final JPanel mainPanel;
     private final JLabel textLabel;
     private final JTextField textField;
-    private final JLabel timeLabels[];
+    private final JLabel timeLabel;
     private final GridLayout layout;
-     
-    private final static String sentence = "The quick, brown, and sneaky fox jumps over the lazy dog";
-    private ArrayList<Character> sentenceSequence = new ArrayList<Character>();
+
+    private final LinkedList<String[]> allWords;
+
     private int lastSpace = 0; //temp value
     private String textString = "";
- 
+
     public TypeRecorder() {
- 
+
         setTitle("Type Recorder");
         setSize(800, 800);
         setLocation(200, 200);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
- 
+
+        this.allWords = getAllWords(3, 9);
+
         this.mainPanel = new JPanel();
-        this.textLabel = new JLabel(sentence);
-        this.timeLabels = new JLabel[sentence.length()];
+        this.textLabel = new JLabel("");
         this.textField = new JTextField(30);
- 
-        this.layout = new GridLayout(sentence.length() + 2, 1);
+        this.timeLabel = new JLabel("");
+
+        this.layout = new GridLayout(3, 1);
         this.layout.setVgap(-5); //crypto wizard magic
         this.mainPanel.setLayout(this.layout);
- 
-        showMainScreen();
-    }
- 
-    private void showMainScreen() {
- 
-        this.textLabel.setText(sentence);
- 
+
         this.mainPanel.add(this.textLabel);
         this.mainPanel.add(this.textField);
+        this.mainPanel.add(this.timeLabel);
         this.mainPanel.setVisible(true);
-        
-        for(int i = 0; i < sentence.length(); i++) {
-           
-        	this.sentenceSequence.add(sentence.charAt(i));
-        	
-        	if(sentence.charAt(i) == ' ' || i + 1 == sentence.length()){
-        		
-        		for(int j = lastSpace; j < i + 1; j++){
-        			
-        			textString += sentence.charAt(j);
-        			
-        		}
-        		
-        		this.timeLabels[i] = new JLabel(textString);
-        		this.mainPanel.add(this.timeLabels[i]);
-        		
-        		lastSpace = i;
-        		textString = "";
-        		
-        	}
-        	
-        }
- 
+
         add(this.mainPanel);
         setVisible(true);
- 
-        new Thread(new TimeTracker(sentence, this.textField, this.timeLabels)).start();
+
+        new Thread(new TimeTracker(this.allWords, this.textLabel, this.textField, this.timeLabel)).start();
     }
- 
-    private static class TimeTracker implements Runnable {
- 
-        private final String text;
-        private final JTextField textField;
-        private final JLabel[] labels;
- 
-        public TimeTracker(String text, JTextField textField, JLabel[] labels) {
-            this.text = text;
-            this.textField = textField;
-            this.labels = labels;
+
+    private static LinkedList<String[]> getAllWords(final int minLetters, final int maxLetters) {
+
+        final String suffix = "_Character_Words.txt";
+        final LinkedList<String[]> allWords = new LinkedList<String[]>();
+
+        for(int i = minLetters; i <= maxLetters; i++) {
+
+            final LinkedList<String> words = new LinkedList<String>();
+            final String fileName = String.valueOf(i) + suffix;
+
+            try {
+
+                BufferedReader reader = new BufferedReader(new FileReader(fileName));
+                String line;
+
+                while((line = reader.readLine()) != null) {
+                    words.add(line);
+                }
+
+                reader.close();
+                allWords.add(words.toArray(new String[words.size()]));
+            }
+            catch(Exception e) {
+                e.printStackTrace();
+            }
         }
- 
+        return allWords;
+    }
+
+    private static class TimeTracker implements Runnable {
+
+        private static final int MAX_TIME = 60000; //60 seconds
+
+        private static final Random generator = new Random();
+
+        private final LinkedList<String[]> allWords;
+        private final JLabel textLabel;
+        private final JTextField textField;
+        private final JLabel timeLabel;
+
+        public TimeTracker(LinkedList<String[]> allWords, JLabel textLabel, JTextField textField, JLabel timeLabel) {
+            this.allWords = allWords;
+            this.textLabel = textLabel;
+            this.textField = textField;
+            this.timeLabel = timeLabel;
+        }
+
         public void run() {
- 
-            long lastTime = 0;
-            int prevLength = 0;
- 
+
+            long startTime = 0;
+            long wordTime = 0;
+            int letterCounter =  0; //First number of letters in the array
+
+            String word = null;
             while(true) {
- 
-                String text = this.textField.getText();
- 
-                final int index;
- 
-                if(text == null || text.length() == 0) {
-                    index = -5;
+
+
+                if(word == null) {
+                        //Choose a random word from the array of words
+                        String[] wordsToChooseFrom = allWords.get(letterCounter);
+                        word = wordsToChooseFrom[generator.nextInt(wordsToChooseFrom.length)];
+                        this.textLabel.setText(word);
                 }
-                else {
-                    index = text.length() - 1;
+
+
+                //As soon as the first letter is typed, start counting
+                if(this.textField.getText() != null && this.textField.getText().length() == 0) {
+
+                    //If we haven't yet initialized start time, do so
+                    if(startTime == 0) {
+                        startTime = System.currentTimeMillis();
+                    }
+
+                    if(wordTime == 0) {
+                        wordTime = System.currentTimeMillis();
+                    }
                 }
- 
-                if(lastTime == 0 && text != null && text.length() == 1) {
-                    lastTime = System.currentTimeMillis();
+
+                if(this.textField.getText() != null && this.textField.getText().equals(word)) {
+
+                    long timeForWord = System.currentTimeMillis() - wordTime;
+                    System.out.println("WORD:\t" + word + "\tTIME:\t" + timeForWord);
+
+                    wordTime = System.currentTimeMillis();
+                    letterCounter++;
+
+                    if(letterCounter == allWords.size()) {
+                        letterCounter = 0;
+                    }
+
+                    this.textField.setText("");
+                    word = null;
                 }
- 
-                if(index >= 0 && index < this.text.length() && text.charAt(index) == this.text.charAt(index) && index != prevLength) {
-                    long time = System.currentTimeMillis() - lastTime;
-                    this.labels[index].setText("Letter: '" + this.text.charAt(index) + "' Time in Nanoseconds: " + time);
-                    lastTime = System.currentTimeMillis();
-                    prevLength = index;
+
+                if(System.currentTimeMillis() - startTime == MAX_TIME) {
+                    this.textField.setEditable(false);
+                    this.textLabel.setText("FINISHED PROGRAM");
                 }
- 
+
+                this.timeLabel.setText("Word time elapsed in MS: " + (System.currentTimeMillis() - wordTime) + ". Game time elapsed in MS: " + (System.currentTimeMillis() - startTime));
+
             }
         }
     }
- 
- 
+
+
     public static void main(String[] ryan) {
- 
+
         TypeRecorder recorder = new TypeRecorder();
         recorder.show();
     }
